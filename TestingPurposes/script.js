a23018f2d4e94bf882c26e25f00061ee4db8d1ff
Paweł Kurantowicz <64223655+pkky@users.noreply.github.com>
2024-04-13 22:05:52 +0200

    Delete TestingPurposes/script.js

--- TestingPurposes/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const nav = document.querySelector('.nav-mobile');
-    const navList = document.querySelector('.nav-list');
-    
-    nav.addEventListener('click', function () {
-        navList.classList.toggle('nav-active');
-    });
-});
